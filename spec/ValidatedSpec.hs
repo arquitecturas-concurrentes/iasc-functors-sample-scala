3ec14927c94d1c43e9c3766f8355ccc9ed02f393
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-07 15:22:30 -0300

    Adding Functor instance

--- spec/ValidatedSpec.hs
+++ spec/ValidatedSpec.hs
@@ -5,6 +5,13 @@ import           Data.Validated
 
 spec :: Spec
 spec = do
-  describe "Validated" $ do
-    it "smoke test" $ do
-      1 `shouldBe` 1
+  describe "fmap" $ do
+    it "should answer valid for valid" $ do
+      fmap (+1) (Valid 1) `shouldBe` (Valid 2)
+
+    it "should answer invalid for invalid" $ do
+      fmap (+1) (Invalid ["foo", "bar"]) `shouldBe` (Invalid ["foo", "bar"])
+
+    it "should answer doubtful for doubtful" $ do
+      fmap (+1) (Doubtful 2 ["foo"]) `shouldBe` (Doubtful 3 ["foo"])
+
--- src/Data/Validated.hs
+++ src/Data/Validated.hs
@@ -1,3 +1,15 @@
 module Data.Validated where
 
-  data Validated = Valid | Invalid
+data Validated a = Valid a
+                 | Doubtful a [String]
+                 | Invalid [String] deriving (Eq, Show)
+
+messages (Valid _) = []
+messages (Doubtful _ ms) = ms
+messages (Invalid ms) = ms
+
+instance Functor Validated where
+  fmap f (Valid a) = Valid (f a)
+  fmap f (Doubtful a ms) = Doubtful (f a) ms
+  fmap _ (Invalid ms) = (Invalid ms)
+
