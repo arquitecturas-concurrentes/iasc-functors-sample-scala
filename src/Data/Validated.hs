de1938c28975a8f26643f6c92a1ff69d205811b7
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-07 15:45:56 -0300

    Removing some duplications

--- src/Data/Validated.hs
+++ src/Data/Validated.hs
@@ -17,14 +17,11 @@ instance Functor Validated where
   fmap f (Doubtful a ms) = Doubtful (f a) ms
   fmap _ (Invalid ms) = (Invalid ms)
 
-
 instance Applicative Validated where
   (Valid f)       <*> v                = fmap f v
   (Doubtful f ms) <*> (Valid v)        = Doubtful (f v) ms
   (Doubtful f ms) <*> (Doubtful v ms2) = Doubtful (f v) (ms2 ++ ms)
-  (Doubtful f ms) <*> (Invalid ms2)    = Invalid  (ms2 ++ ms)
-  (Invalid ms)    <*> (Valid _)        = Invalid  ms
-  (Invalid ms)    <*> (Doubtful _ ms2) = Invalid  (ms2 ++ ms)
-  (Invalid ms)    <*> (Invalid  ms2)   = Invalid  (ms2 ++ ms)
+  v               <*> v2               = Invalid $ (messages v2) ++ (messages v)
 
   pure v = Valid v
+
